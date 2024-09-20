183d5912c7496963415d1c12af78a15b9bc41cf8
chaitanya-codes <chaitanyabajaj12@gmail.com>
2024-09-21 00:54:37 +0530

    MINOR RELEASE

--- src/app/components/ResearchProjects.tsx
+++ src/app/components/ResearchProjects.tsx
@@ -7,30 +7,37 @@ function selectProject(project: string) {
     }
 }
 
+function Capsule(text: any) {
+    return (
+        <div className="inline rounded-[80px] p-2 bg-[rgba(86,_205,_173,_0.10)]">{text}</div>
+    )
+}
+
 function getProjects() {
     const projects = ["Quantum Dot-Assisted Photonic Signal ", "AI-Driven Traffic Flow Optimization", "Biosensor-Enhanced Disease Detection", "Graphene-Based Flexible Electronics",
         "Data-Driven Predictive Healthcare", "Cognitive Load Assessment Virtual Reality", "CRISPR-Cas9 Mediated Gene Editing", "Smart City Development Through IoT"];
 
     return projects.map((project, index) => (
         <div key={index} className='py-10 px-16 bg-white m-5'>
-            <svg className="mb-2" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
-                <g clip-path="url(#clip0_69_18258)">
-                    <path d="M6 42.0001H14L40 16.0001C41.0609 14.9393 41.6569 13.5004 41.6569 12.0001C41.6569 10.4998 41.0609 9.06098 40 8.00012C38.9391 6.93925 37.5003 6.34326 36 6.34326C34.4997 6.34326 33.0609 6.93925 32 8.00012L6 34.0001V42.0001Z" stroke="#4640DE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M29 11L37 19" stroke="#4640DE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M24 16L14 6L6 14L16 24" stroke="#4640DE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M14 16L11 19" stroke="#4640DE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M32 24L42 34L34 42L24 32" stroke="#4640DE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M32 34L29 37" stroke="#4640DE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+            <svg className="inline mr-4" xmlns="http://www.w3.org/2000/svg" width="49" height="58" viewBox="0 0 49 58" fill="none">
+                <g clip-path="url(#clip0_69_21370)">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0 14.4938V42.9387L24.4321 57.6283L24.9992 56.7151L24.4321 29.1084L0.840059 14.5107L0 14.4938Z" fill="#449B82" />
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M48.5995 14.3602V43.2058L24.4316 57.6285V29.1085L47.7143 14.3819L48.5995 14.3602Z" fill="#9BDB9C" />
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M24.3 0.20401L48.6 14.3597L24.4321 29.5839L0 14.4933L24.3 0.20401Z" fill="#56CDAD" />
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M36.5063 11.5483L28.2876 16.413V26.2319L20.053 21.2873L12.1348 25.9742V46.497L20.3535 41.4004V30.3534L29.1317 35.957L36.5063 31.3839V11.5483Z" fill="white" />
                 </g>
                 <defs>
-                    <clipPath id="clip0_69_18258">
-                        <rect width="48" height="48" fill="white" />
+                    <clipPath id="clip0_69_21370">
+                        <rect width="48.6" height="57.6" fill="white" transform="translate(0 0.200012)" />
                     </clipPath>
                 </defs>
             </svg>
-            <button onClick={() => selectProject(project)}>
+            <button className="inline" onClick={() => selectProject(project)}>
                 <p className='font-russo text-lg mb-2'>{project}</p>
             </button>
+            <div className='mt-3'>
+            {Capsule('Full-Time')}
+            </div>
         </div>
     ))
 }
