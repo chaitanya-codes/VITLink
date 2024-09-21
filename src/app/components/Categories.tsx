280aeb2cefb067b30d7b984042a4d0aba825b813
chaitanya-codes <chaitanyabajaj12@gmail.com>
2024-09-21 13:12:46 +0530

    hover color

--- src/app/components/Categories.tsx
+++ src/app/components/Categories.tsx
@@ -10,8 +10,8 @@ function selectCategory(category: string) {
 function getCategories(categories) {
 
     return categories.map((category, index) => (
-        <div key={index} className='py-14 px-16 bg-white m-5'>
-            <svg className="mb-6" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <div key={index} className='py-14 px-16 bg-white hover:bg-[#312bea] m-5'>
+            <svg className="mb-6 " xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
                 <g clipPath="url(#clip0_69_18258)">
                     <path d="M6 42.0001H14L40 16.0001C41.0609 14.9393 41.6569 13.5004 41.6569 12.0001C41.6569 10.4998 41.0609 9.06098 40 8.00012C38.9391 6.93925 37.5003 6.34326 36 6.34326C34.4997 6.34326 33.0609 6.93925 32 8.00012L6 34.0001V42.0001Z" stroke="#4640DE" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M29 11L37 19" stroke="#4640DE" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
