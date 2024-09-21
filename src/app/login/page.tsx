b4738f0cc5e54752883e8a5bb4bf8abf9cbee985
speeding falcon <arkaappcontrol@gmail.com>
2024-09-21 15:49:44 +0530

    finished login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,8 +15,8 @@ const Logo = () => (
 const Jobby = () => (
 	<Image
 	  src={img2}// Corrected path, relative to the public folder
-	  height={600} // Desired height
-	  width={300} // Desired width
+	  height={700} // Desired height
+	  width={500} // Desired width
 	  alt="Man"
 	/>
 );
@@ -26,16 +26,16 @@ export default function NextPage() {
         <Navbar showLogin ={false}/>
         <div className="flex">
           <div className="pbox">
-            <div className="photu mt-300 px-50 mb-0">
+            <div className="photu  mb-0">
             <Jobby></Jobby>
             </div>
           </div>
           <div className="lbox container px-20">
-            <div className="2buts flex">
+            <div className="2buts flex ml-20">
               <button className="rbut">JOB SEEKER</button>
               <button className="lbut">COMPANY</button>
             </div>
-            <p className="bigText">Get, Create More Opportunities</p>
+            <p className="bigText py-10">Get, Create More Opportunities</p>
             <button className="Vbut flex">
               <div><Logo></Logo></div>
               <div className="vitText"><p >Log in with VIT Mail</p></div>
