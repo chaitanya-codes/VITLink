d1a0708e2d36852367f9697619f746727c0bfaf1
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-09-22 02:33:20 +0530

    Added styles

--- src/app/sign_in/page.tsx
+++ src/app/sign_in/page.tsx
@@ -1,7 +1,25 @@
-import { signIn } from "@/auth"
+import { auth, signIn, signOut } from "@/auth"
 
-export default function SignIn() {
-    return (
+export default async function SignIn() {
+  const session = await auth();
+  console.log(session)
+  const user = session?.user
+  return user?
+  (
+    <>
+  <h1 className="font-bold">Welcome </h1>
+  <div className="mx-5">{user.name}</div>
+  <form
+        action={async () => {
+          "use server"
+          await signOut()
+        }}
+      >
+        <button className="google-signin" type="submit">Sign out</button>
+      </form>
+  </>
+)
+  :(
       <form
         action={async () => {
           "use server"
